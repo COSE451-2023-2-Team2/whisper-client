206d45e77b2dd1b5aed53cf48a0990039b0bdbd7
everGreenGH <skin0011@naver.com>
2023-11-01 15:00:57 +0900

    feat: ButtonModal 추가

--- src/components/atoms/button/ButtonSubmit/index.tsx
+++ src/components/atoms/button/ButtonSubmit/index.tsx
@@ -5,7 +5,7 @@ import Button from "../Button";
 export default function ButtonSubmit(props: ComponentProps<typeof Button>) {
   return (
     <Button
-      className={s.button_base}
+      className={s.button_submit}
       disabled={props.disabled}
       onClick={props.onClick}
     >
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,19 +3,23 @@ import s from "./index.module.scss";
 import Background from "@/public/assets/background.png";
 import Image from "next/image";
 import Header from "@/components/layouts/Header";
+import ButtonSubmit from "@/components/atoms/button/ButtonSubmit";
+import ButtonModal from "@/components/atoms/button/ButtonModal";
 
 export default function Login() {
   return (
     <div className={s.login}>
       <Header></Header>
       <div className={s.login_content}>
-        <LoginBox></LoginBox>
+        {/* <LoginBox></LoginBox> */}
         {/* <Image
           src={Background}
           alt="background"
           width={650}
           height={500}
         ></Image> */}
+        {/* <ButtonSubmit name="test"></ButtonSubmit>
+        <ButtonModal name="test"></ButtonModal> */}
       </div>
     </div>
   );
