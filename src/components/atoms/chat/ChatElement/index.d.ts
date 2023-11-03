c8bacb2cd3fa4b123c002e65854107fba2f6127f
everGreenGH <skin0011@naver.com>
2023-11-03 16:09:41 +0900

    feat: chat element 추가

---
+++ src/components/atoms/chat/ChatElement/index.d.ts
@@ -0,0 +1,4 @@
+export interface ChatElementProps {
+  isMine: boolean;
+  message: string;
+}
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,3 +1,12 @@
+import ChatElement from "@/components/atoms/chat/ChatElement";
+import ChatTime from "@/components/atoms/chat/ChatTime";
+
 export default function Chat() {
-  return <div>Chat...</div>;
+  return (
+    <div>
+      {/* <ChatElement isMine={false} message="안녕하세요!"></ChatElement>
+      <ChatElement isMine={false} message="Race condition에 대해 물어봐도 되나요?"></ChatElement>
+      <ChatTime time={new Date()}></ChatTime> */}
+    </div>
+  );
 }
