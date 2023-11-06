966777de8d2aaad9d133efb2f3190ad5a6b40e40
everGreenGH <skin0011@naver.com>
2023-11-06 17:44:20 +0900

    feat: add username UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,18 +5,26 @@ import Header from "@/components/layouts/Header";
 import LoginTemplate from "@/components/templates/LoginTemplate";
 import Popup from "@/components/popup";
 import useSocket from "@/hooks/useSocket";
+import ChatElement from "@/components/atoms/chat/ChatElement";
 
 export default function Login() {
   useSocket();
 
   return (
-    <div className={s.login}>
-      <Header></Header>
-      <Popup></Popup>
-      <div className={s.login_content}>
-        <LoginTemplate></LoginTemplate>
-        <Image className={s.login_image} src={Background} alt="background" width={650} height={500}></Image>
-      </div>
-    </div>
+    // <div className={s.login}>
+    //   <Header></Header>
+    //   <Popup></Popup>
+    //   <div className={s.login_content}>
+    //     <LoginTemplate></LoginTemplate>
+    //     <Image className={s.login_image} src={Background} alt="background" width={650} height={500}></Image>
+    //   </div>
+    // </div>
+    <ChatElement
+      isMine={false}
+      isFirst={true}
+      isLast={true}
+      userName={"Pengtoshi Nakamoto"}
+      message={"테스트 메시지"}
+    ></ChatElement>
   );
 }
